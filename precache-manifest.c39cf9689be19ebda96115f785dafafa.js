2ca82d65822f2ce9325d7fb90365f0899e12f0f5
Marina Semenova <marina.semenova@student.tdsb.on.ca>
2019-07-02 01:28:54 -0400

    Updates

---
+++ precache-manifest.c39cf9689be19ebda96115f785dafafa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58711af8726259382bbd8b711ebd06f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2c6d9993199f757e707",
+    "url": "/static/css/main.2a380fa0.chunk.css"
+  },
+  {
+    "revision": "cb40ce857687cbc01cc0",
+    "url": "/static/js/2.e66fc554.chunk.js"
+  },
+  {
+    "revision": "d2c6d9993199f757e707",
+    "url": "/static/js/main.1f0c622c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
